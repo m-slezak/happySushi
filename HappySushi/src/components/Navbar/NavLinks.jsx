0a9483eeca12b6f1d92b68f9a9eba2fde369301a
m-slezak <77272905+m-slezak@users.noreply.github.com>
2023-03-10 14:19:26 +0100

    Navigation ul styles

--- HappySushi/src/components/Navbar/NavLinks.jsx
+++ HappySushi/src/components/Navbar/NavLinks.jsx
@@ -2,12 +2,39 @@ import React from "react";
 import { motion } from "framer-motion";
 
 const NavLinks = () => {
+  const animateFrom = { opacity: 0, y: -40 };
+  const animateTo = { opacity: 1, y: 0 };
+
   return (
     <ul>
-      <li>O nas</li>
-      <li>Menu</li>
-      <li>Kontakt</li>
-      <li>Aktualności</li>
+      <motion.li
+        initial={animateFrom}
+        animate={animateTo}
+        transition={{ delay: 0.1 }}
+      >
+        O nas
+      </motion.li>
+      <motion.li
+        initial={animateFrom}
+        animate={animateTo}
+        transition={{ delay: 0.2 }}
+      >
+        Menu
+      </motion.li>
+      <motion.li
+        initial={animateFrom}
+        animate={animateTo}
+        transition={{ delay: 0.3 }}
+      >
+        Kontakt
+      </motion.li>
+      <motion.li
+        initial={animateFrom}
+        animate={animateTo}
+        transition={{ delay: 0.4 }}
+      >
+        Aktualności
+      </motion.li>
     </ul>
   );
 };
