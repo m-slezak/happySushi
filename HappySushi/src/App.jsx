618213374251d38938052dc8ffb84b0774266deb
m-slezak <77272905+m-slezak@users.noreply.github.com>
2023-03-12 15:28:20 +0100

    add background img

--- HappySushi/src/App.jsx
+++ HappySushi/src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar/Navbar";
+import About from "./components/About/About";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <About />
     </div>
   );
 }
---
+++ HappySushi/src/components/About/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../About/about.module.css";
+
+const About = () => {
+  return (
+    <section className={styles.About}>
+      <h1>ELO</h1>
+    </section>
+  );
+};
+
+export default About;
